e9126362ab82751492f5cc03187e092d24d04b58
jabezinu <jabezzinu31@gmail.com>
2025-11-21 23:49:05 +0300

    common backend root

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
-export const BACKEND_URL = 'http://localhost:5001';
+// export const BACKEND_URL = 'http://localhost:5001';
+export const BACKEND_URL = 'https://bea.kalgemstones.com';
 export const API_BASE_URL = `${BACKEND_URL}/api`;
