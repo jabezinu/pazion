019f90731b69eab51e91daad4a2963d3221594bb
jabezinu <jabezzinu31@gmail.com>
2025-11-26 08:14:58 +0300

    development mode

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,3 +1,2 @@
-// export const API_BASE_URL = 'https://be.kalgemstones.com/api';
-// export const API_BASE_URL = 'http://localhost:5001/api';
-export const API_BASE_URL = 'https://bec.kalgemstones.com/api';
+export const API_BASE_URL = 'http://localhost:5001/api';
+// export const API_BASE_URL = 'https://bec.kalgemstones.com/api';
--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
-// export const BACKEND_URL = 'http://localhost:5001';
-export const BACKEND_URL = 'https://bec.kalgemstones.com';
-export const API_BASE_URL = `${BACKEND_URL}/api`;
+export const API_BASE_URL = `http://localhost:5001/api`;
+// export const API_BASE_URL = `https://bec.kalgemstones.com/api`;
